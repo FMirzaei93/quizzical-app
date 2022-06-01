f5df3cf4909563958d040313d9df1dbec9f36069
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-05-31 20:03:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Question from "./components/Question";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [dataArray, setDataArray] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=5")
+      .then((response) => response.json())
+      .then((data) => setDataArray(data.results));
+  }, []);
+
+  const questionsArray = dataArray.map((item) => {
+    // Adding id to the existing objects (elements of the fetched array)
+    return {
+      ...item,
+      id: nanoid(),
+    };
+  });
+
+  function shuffleArray(array) {
+    return array.sort(() => Math.random() - 0.5);
+  }
+
+  const questionComponentsArray = questionsArray.map((item) => {
+    return (
+      <Question
+        key={item.id}
+        question={item.question}
+        answers={shuffleArray([...item.incorrect_answers, item.correct_answer])}
+        correct_answer={item.correct_answer}
+        incorrect_answers={item.incorrect_answers}
+        id={item.id}
+      />
+    );
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='main-container'>
+      <div> {questionComponentsArray}</div>
+
+      <button className='check-button'>Check answers</button>
+    </main>
   );
 }
 
