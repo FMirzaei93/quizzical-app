8e9a6a8655bf2e92f11f03d2a9eb679c459af6c9
FMirzaei93 <firoozeh.mirzaei@gmail.com>
2022-08-01 20:11:32 -0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ function App() {
           );
         }
         return response.json();
+        //The json() method of the Response interface takes a Response stream and reads it to completion. It returns a promise which resolves with the result of parsing the body text as JSON.
+        //Note that despite the method being named json(), the result is not JSON but is instead the result of taking JSON as input and parsing it to produce a JavaScript object.
       })
       .then((data) => createObjArray(data.results))
       .then((objsArray) => {
